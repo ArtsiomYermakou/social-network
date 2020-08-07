593436f07e3156874824222fd9a91108c48a0cee
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-08-07 21:29:49 +0300

    Lesson 63 Add DALL(api.tsx)

---
+++ src/api/api.tsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+    headers: {"API-KEY": "298395db-2088-448b-824f-15482d6c3999"},
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data;
+            });
+    }
+}
+
+
+export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+    return instance.get(`follow?page=${currentPage}&count=${pageSize}`)
+        .then(response => {
+            return response.data;
+        });
+}
