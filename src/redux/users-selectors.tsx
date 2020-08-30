5fd6bb87125092cc482244fd53dc269bec38fe0b
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-08-30 20:01:01 +0300

    Lesson 83. Reselect(part_2)

--- src/redux/users-selectors.tsx
+++ src/redux/users-selectors.tsx
@@ -1,18 +1,26 @@
 import {AppStateType} from "./redux-store";
+import {createSelector} from "reselect";
 
-export const getUsers = (state: AppStateType) => {
+const getUsersSelector = (state: AppStateType) => {
     return state.usersPage.users;
 }
+
+export const getUsers = createSelector(getUsersSelector,
+    (users) => {
+        return users.filter((u: any) => true)
+    }
+)
+
 export const getPageSize = (state: AppStateType) => {
     return state.usersPage.pageSize;
 }
-export const getTotalUsersCount= (state: AppStateType) => {
+export const getTotalUsersCount = (state: AppStateType) => {
     return state.usersPage.totalUsersCount;
 }
-export const getCurrentPage= (state: AppStateType) => {
+export const getCurrentPage = (state: AppStateType) => {
     return state.usersPage.currentPage;
 }
-export const getIsFetching= (state: AppStateType) => {
+export const getIsFetching = (state: AppStateType) => {
     return state.usersPage.isFetching;
 }
 export const getFollowingInProgress = (state: AppStateType) => {
