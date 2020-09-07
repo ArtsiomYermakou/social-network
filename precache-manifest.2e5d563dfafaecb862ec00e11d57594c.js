9f6cbbd09498144d104c5ba840cb0ff17f0b2dca
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-09-07 15:42:25 +0300

    Updates

---
+++ precache-manifest.2e5d563dfafaecb862ec00e11d57594c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616e6b660035ae624da14d4595a6a5cc",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "f6acffd0b4140cee06ed",
+    "url": "/social-network/static/css/4.b345c625.chunk.css"
+  },
+  {
+    "revision": "6541a8d47fc7e6a7ec07",
+    "url": "/social-network/static/css/5.0461a141.chunk.css"
+  },
+  {
+    "revision": "c44bcc7815dbf90ed410",
+    "url": "/social-network/static/css/6.460308d7.chunk.css"
+  },
+  {
+    "revision": "0304ee431fc00b525e41",
+    "url": "/social-network/static/css/main.16bf01e0.chunk.css"
+  },
+  {
+    "revision": "932f28bcb5a7a2b740a8",
+    "url": "/social-network/static/js/0.6df02d9c.chunk.js"
+  },
+  {
+    "revision": "232908c9be7f72aeae7d",
+    "url": "/social-network/static/js/3.68ecd198.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/3.68ecd198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6acffd0b4140cee06ed",
+    "url": "/social-network/static/js/4.dabe8cca.chunk.js"
+  },
+  {
+    "revision": "6541a8d47fc7e6a7ec07",
+    "url": "/social-network/static/js/5.0e5ac47d.chunk.js"
+  },
+  {
+    "revision": "c44bcc7815dbf90ed410",
+    "url": "/social-network/static/js/6.d0fe0080.chunk.js"
+  },
+  {
+    "revision": "0304ee431fc00b525e41",
+    "url": "/social-network/static/js/main.dae48a1b.chunk.js"
+  },
+  {
+    "revision": "e48b01d605bf6d6edb8b",
+    "url": "/social-network/static/js/runtime-main.7506cd4d.js"
+  },
+  {
+    "revision": "b2fce85a27f7cd1ddd9998e8283de967",
+    "url": "/social-network/static/media/businessperson-computer-icons-avatar-clip-art-avatar.b2fce85a.jpg"
+  },
+  {
+    "revision": "818b13e57ca0754d4c51e1d93b27924a",
+    "url": "/social-network/static/media/loaderr.818b13e5.svg"
+  }
+]);
